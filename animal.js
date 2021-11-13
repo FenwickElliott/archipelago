0938d7646498d2e78770c718c9d849d36d3a54d4
Charles Fenwick Elliott <charles@fenwickelliott.io>
2021-11-13 16:42:09 -0500

    init

---
+++ animal.js
@@ -0,0 +1,5 @@
+export class Animal {
+    constructor(name) {
+        this._name = name
+    }
+}
---
+++ dog.js
@@ -0,0 +1,4 @@
+import { Animal } from './animal.js';
+
+const a = new Animal('twig');
+console.log(a);
